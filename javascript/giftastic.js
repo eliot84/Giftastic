786fad410f5b5557f0b393a7f292c99d24778d67
Eliot <mr.ear84@gmail.com>
2018-03-05 22:32:08 -0500

    resolved object setup and the gifs now appear dynamically

--- javascript/giftastic.js
+++ javascript/giftastic.js
@@ -1,7 +1,8 @@
 
 
 gifty = {
-			villains: [{villain: "joker"}, {villain: "Captain Hook"}],
+			configurable: true,
+			initiateArray: true,
 			apiKey: "Vs2Ptjtt59FKmnxVaKDxpDUwUAJ8qogj",
 			q: ""
 
@@ -22,27 +23,48 @@ $( "#submitSearch" ).click(function() {
 	 var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + gifty.q + "&api_key=" + gifty.apiKey + "&limit=5");
 			   xhr.done(function(data) { 
 
-			   	console.log("success got data", data);
+			 //  	console.log("success got data", data);
 				
 			   	//access image url
-				console.log(data.data[0].images.fixed_height.url);
+			//	console.log(data.data[0].images.fixed_height.url);
 
 			   	//access rating
-				var rating = data.data[0].rating;
+			//	var rating = data.data[0].rating;
 
 				//access image url
-				var url = data.data[0].images.fixed_height.url;
-				gifty.villains.push({villain: addVillain, rating: rating, url: url }); //add the submitted variable to the villains array in gifty
-/*
-					  	 $("#image").attr("src", url);
+			//	var url = data.data[0].images.fixed_height.url;
+				//gifty.villains.push({villain: addVillain, rating: rating, url: url }); //add the submitted variable to the villains array in gifty
 
-					  	 $("#allImages").append($('<div>', {class: "row", id: "this"}));
-					  	 $(".row").append($('<div>', {class: "col-xs-12 col-sm-6 col-md-4 col-md-3", id: "inHere"}));
-						/*$('#inHere').append($('<img>', {src: url, class: "float-left", id: "pic"}));
-						$('#inHere').append($('<img>', {src: url, class: "float-left", id: "pic"}));
-						$('#inHere').append($('<img>', {src: url, class: "float-left", id: "pic"}));
-						$('#inHere').append($('<img>', {src: url, class: "float-left", id: "pic"})); */
-					
+
+//If the first time adding to an array
+if(gifty.initiateArray){
+Object.defineProperty(gifty, 'villains', { 
+	//configurable: true,
+	value: [{villain: addVillain, rating: data.data[0].rating, animate: data.data[0].images.fixed_height.url}] });
+	
+	for(var i = 1; i < 4; i++){
+		gifty.villains.push({ villain: addVillain, rating: data.data[i].rating, animate: data.data[i].images.fixed_height.url });
+	}
+	gifty.initiateArray = false;
+}
+//not the first time no need to define properties
+else
+{
+
+}
+
+console.log(gifty);
+
+  	 $("#allImages").append($('<div>', {class: "row"}));
+			$('.row').append($('<div>'));
+
+			for(var i = 0; i < 4; i++){
+				$('.row').append($('<img>', {src: gifty.villains[i].animate, class: "col-xs-12 col-sm-3 col-md-3 col-lg-3 float-left"}));
+
+			}
+
+
+
 					//	 $('#pic').attr("src", url); 
 			/*
 				for (var i = 0; i < 4; i++)
--- javascript/sandbox.js
+++ javascript/sandbox.js
@@ -3,21 +3,51 @@
 
 //$('#allImages').append($('<div>', {class: "row"}));
 
-
+/*
   	 $("#allImages").append($('<div>', {class: "row"}));
 			$('.row').append($('<div>'));
-			/*
+			
 				$('.row').append($('<div>', {id: "red", class: "col-xs-12 col-sm-6 col-md-4 col-lg-3 float-left"}));
 				$('.row').append($('<div>', {id: "red", class: "col-xs-12 col-sm-6 col-md-4 col-lg-3 float-left"}));
 				$('.row').append($('<div>', {id: "red", class: "col-xs-12 col-sm-6 col-md-4 col-lg-3 float-left"}));
 				$('.row').append($('<div>', {id: "red", class: "col-xs-12 col-sm-6 col-md-4 col-lg-3 float-left"}));
 			*/		
-
+/*
 		$('.row').append($('<img>', {class: "float-left", src: "images/pic.gif"}));
 		$('.row').append($('<img>', {class: "float-left", src: "images/pic.gif"}));
 		$('.row').append($('<img>', {class: "float-left", src: "images/pic.gif"}));
 		$('.row').append($('<img>', {class: "float-left", src: "images/pic.gif"}));
 
-		
+	*/	
 
 console.log("yyo");
+/*
+const gifty = {};
+
+Object.defineProperty(gifty, 'villains', { value: [{villain: "joker", url: "www"}] }); 
+Object.defineProperty(gifty, 'villains', { value2: [{villain: "catwoman", url: "wwww"}] }); 
+Object.defineProperty(gifty, 'villains', { value3: [{villain: "catwoman", url: "wwww"}] }); 
+*/
+
+const object1 = {villains: {
+	configurable: true
+}};
+
+Object.defineProperty(object1, 'villains', { 
+	//configurable: true,
+	value: [{villain: "Ursela", rating1: "xxx", animate1: "yyy"}] });
+//Object.defineProperty(object1, 'villains', {value: [{villain: "Hook", url: "wwww"}] });
+
+	
+//object1.villains.push({villain: "hook",  url: "xxx" });
+
+
+
+
+console.log(object1.villains[0].villain);
+
+
+console.log(object1);
+
+
+//Object.defineProperty(object1, 'value', {value: [{value: 42, writable: false}] });
